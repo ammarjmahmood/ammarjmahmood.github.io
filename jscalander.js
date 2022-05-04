64654841a5c7cd867af55a6b9c55b416e72e2b75
Ammar Mahmood <ammarmahmood288@gmail.com>
2022-05-04 13:52:22 -0400

    date code again

--- jscalander.js
+++ jscalander.js
@@ -376,7 +376,8 @@ function checkTime(i) {
 
 function dayontimetable() {
     const todayDate = new Date();
-    const timetableday = todayDate % 2
+    const todayDay = todayDate.getDay();
+    const timetableday = todayDay % 2
     if (timetableday == 0) {
         document.getElementById('timetabledayprint').innerHTML = "Day 1";
       } 
