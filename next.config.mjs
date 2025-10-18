1aa7c861270a1bec2860590a415823e959f5bfcd
Ammar Jaleel Mahmood <85653702+ammarjmahmood@users.noreply.github.com>
2025-10-18 17:51:18 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
   experimental: {
     optimizePackageImports: ['lucide-react'],
   },
